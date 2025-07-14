b05408c153b68ac5d3aef7ba25252ed410c3498b
FGSSmap <fgss.map@gmail.com>
2025-07-14 09:56:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+// 投稿ボタンのクリックイベントでデータ送信
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {
+    author: form.author.value,
+    faculty: form.faculty.value,
+    title: form.title.value,
+    description: form.description.value,
+    timestamp: new Date()
+  };
+  firebase.firestore().collection("memories").add(data);
+});
